fb1836b455667f3df36d47b32be0465e4e0ddc97
dmorsecode <morsecoding@protonmail.com>
2025-02-14 23:55:10 -0500

    Add test Shadcn components to verify it still works after Tailwind migration.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,12 @@
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
+	import { Toaster } from "$lib/components/ui/sonner";
+
 	let { children } = $props();
 </script>
+
+<Toaster />
 
 <ParaglideJS {i18n}>
 	{@render children()}
