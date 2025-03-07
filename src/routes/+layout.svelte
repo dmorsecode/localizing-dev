a6d7b5ab7c86c54b6799d0f29dd800fc9a991664
dmorsecode <morsecoding@protonmail.com>
2025-03-06 21:34:30 -0500

    Added header and footer components and necessary layouts.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,23 @@
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
-	import { Toaster } from "$lib/components/ui/sonner";
+	import { Toaster } from '$lib/components/ui/sonner';
+	import Header from '$lib/components/layout/header.svelte';
+	import Footer from '$lib/components/layout/footer.svelte';
 
-	let { children } = $props();
+	import type { LayoutProps } from './$types';
+
+	let { data, children }: LayoutProps = $props();
 </script>
 
 <Toaster />
 
 <ParaglideJS {i18n}>
-	{@render children()}
+	<div class="flex flex-col min-h-[100vh]">
+	<Header user={data.user} />
+	<main class="m-12 grow">
+		{@render children()}
+	</main>
+	<Footer />
+	</div>
 </ParaglideJS>
