81c458ec806cc2551bfa71991e12a14626d98082
dmorsecode <morsecoding@protonmail.com>
2025-03-06 21:33:45 -0500

    Added repository card component.

---
+++ src/lib/components/display/repos/repo-card.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+	import * as Avatar from '$lib/components/ui/avatar';
+	import * as Table from "$lib/components/ui/table";
+	import { Separator } from "$lib/components/ui/separator";
+	import { Button } from "$lib/components/ui/button";
+	import { GitPullRequest } from 'lucide-svelte';
+
+	export let repo;
+</script>
+
+<Card.Root class="shadow-sm border-gray-600/15 w-96 text-sm">
+	<a href="{repo.html_url}">
+	<Card.Header class="p-2 pb-0 flex flex-row items-start gap-2">
+		<Avatar.Root>
+			<Avatar.Image src="{repo.owner.avatar_url}" alt="{repo.owner.login}" />
+			<Avatar.Fallback>CN</Avatar.Fallback>
+		</Avatar.Root>
+		<div>
+			<Card.Title>{repo.name}</Card.Title>
+			<Card.Description>{repo.owner.login}</Card.Description>
+		</div>
+	</Card.Header>
+	</a>
+	<Card.Content class="p-3 pt-0">
+		<p class="h-12">{repo.description ?? "No description available."}</p>
+		<br/>
+		<Table.Root class="text-xs">
+			<Table.Header>
+				<Table.Row>
+					<Table.Head class="px-0">Original</Table.Head>
+					<Table.Head class="px-0">Requested</Table.Head>
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+				<Table.Row>
+					<Table.Cell class="px-0 align-top">English</Table.Cell>
+					<Table.Cell class="px-0">Spanish<br/>Chinese<br/>Portuguese<br/>Russian</Table.Cell>
+				</Table.Row>
+			</Table.Body>
+		</Table.Root>
+	</Card.Content>
+	<Separator class="" />
+	<Card.Footer class="p-2 flex justify-between">
+		<p class={`${repo.license == null ? "italic text-primary/60" : ""}`}>{repo.license?.name ?? "No license."}</p>
+		<Button href={`${repo.html_url + "/fork"}`} target="_blank" rel="noopener noreferrer" variant="outline">
+			<GitPullRequest size={16} class="mr-2" />
+			Contribute
+		</Button>
+	</Card.Footer>
+</Card.Root>
---
+++ src/lib/components/display/repos/repo-grid.svelte
@@ -0,0 +1,14 @@
+<script>
+	import RepoCard from '$lib/components/display/repos/repo-card.svelte';
+
+	export let repos;
+</script>
+
+<div class="grid grid-cols-[repeat(auto-fill,minmax(400px,1fr))] gap-6 place-items-center">
+	{#if repos?.length === 0}
+		<p class="text-center text-muted-foreground">No repositories found.</p>
+	{/if}
+	{#each repos as repo}
+		<RepoCard repo={repo} />
+	{/each}
+</div>
