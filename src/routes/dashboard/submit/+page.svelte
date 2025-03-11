bfbe6c9c40823d74fa2df9cfd2e8299c214018ef
dmorsecode <morsecoding@protonmail.com>
2025-03-11 06:08:22 -0400

    Created repo submission form component. Adds new dependencies as required by Shadcn.

---
+++ src/routes/dashboard/submit/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PageData } from './$types.js';
+	import SettingsForm from '$lib/components/forms/repo-submission-form/repo-submission-form.svelte';
+
+	export let data: PageData;
+</script>
+
+<div class="w-1/3 border shadow-md rounded-md p-12 m-auto">
+	<SettingsForm data={data.form} repos={data.repos} />
+</div>
